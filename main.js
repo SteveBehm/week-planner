0e68a43087568f319e90d2b151079d21aa4b68da
Justin Son <justintaeson@gmail.com>
2022-02-18 14:52:00 -0800

    Justin 's 2nd Time.

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
 var $addEntry = document.querySelector('.add-entry');
+var $submitButton = document.querySelector('.submit-entry');
 
 $addEntry.addEventListener('click', switchToFlex);
+$submitButton.addEventListener('click', handleSubmit);
 
 function switchToFlex(event) {
   document.querySelector('.bg-modal').style.display = 'flex';
+}
+
+function handleSubmit(event) {
+  document.querySelector('.bg-modal').style.display = 'none';
 }
