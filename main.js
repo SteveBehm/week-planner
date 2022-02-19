fd4a89cde961dee77395055005b8d786b5d20d0f
Steve Behm <stevtbehm1@gmail.com>
2022-02-18 16:58:12 -0800

    steve third session

--- main.js
+++ main.js
@@ -1,13 +1,20 @@
 var $addEntry = document.querySelector('.add-entry');
-var $submitButton = document.querySelector('.submit-entry');
+var $submitForm = document.querySelector('form');
 
 $addEntry.addEventListener('click', switchToFlex);
-$submitButton.addEventListener('click', handleSubmit);
+$submitForm.addEventListener('submit', handleSubmitEntry);
 
 function switchToFlex(event) {
   document.querySelector('.bg-modal').style.display = 'flex';
 }
 
-function handleSubmit(event) {
+var $secondView = document.querySelector('.second-view');
+var $mainPage = document.querySelector('.main-page');
+
+function handleSubmitEntry(event) {
+  event.preventDefault();
+  $mainPage.className = 'container font-family main-page hidden';
+  $secondView.className = 'container second-view font-family';
+
   document.querySelector('.bg-modal').style.display = 'none';
 }
